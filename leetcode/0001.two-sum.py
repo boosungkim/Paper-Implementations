9b8895efac6a0f63775fca966f9bd4f6c8c49d3b
Boosung Kim <boosung@boosungkim.com>
2023-06-18 00:40:36 +0900

    Delete leetcode directory

--- leetcode/0001.two-sum.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prev_sum = {}
-
-        for i,num in enumerate(nums):
-            if num in prev_sum: return [prev_sum[num],i]
-            prev_sum[target-num] = i
